5c3a68ed71a7ac8e257871840bc28f382b77bca5
Peyman <p.zonouz@gmail.com>
2024-09-16 11:30:53 +0330

    react router and MUI implemented.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import PrimarySearchAppBar from "./components/PrimarySearchAppBar";
+import "./index.css";
+import TasksPage from "./components/TasksPage";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <PrimarySearchAppBar />,
+    children: [{ path: "/tasks", element: <TasksPage /> }],
+  },
+]);
+createRoot(document.getElementById("root")!).render(
+  <RouterProvider router={router} />
+);
